cd36a727f7e6080740ebf46999d56dfff31df25c
Hyeeun92 <21545@student.dorset-college.ie>
2021-01-26 18:41:49 +0000

    1

---
+++ main.js
@@ -0,0 +1,22 @@
+var name = Hyeeun;
+var myAlphabet = ['A','B','C','D','E','F','G'];
+
+function myAlphabetLenghth() {
+    console.log(myAlphabet.length);
+}
+var Planets = ['a','b','c','d','e'];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+var wowDatatypes = ['a',34, 59872655];
+console.log(wowDatatypes);
+
+var hello = function(){
+    return 'Hello world!';
+}
+console.log(hello);
+
+
