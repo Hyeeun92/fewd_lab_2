1760718f139b2df16b64fa7dbe008e257dc97d78
Hyeeun92 <21545@student.dorset-college.ie>
2021-01-26 18:57:14 +0000

    2

--- main.js
+++ main.js
@@ -17,10 +17,3 @@ var hello = function(){
     return 'Hello world!';
 }
 console.log(hello);
-
-var a = 5, b = 10, c = "5";
-console.log(a==c);
-console.log(a===c);
-console.log(a!=b);
-console.log(a>=c );
-
